2af59ed087f3f835f82d39e818406f1b44452f8d
s poorna chandra <poornanaidu1@gmail.com>
2023-12-06 09:21:25 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source   = "git::https://github.com/SPOORNACHANDRA/tf-module-vpc.git"
+  source   = "git::https://github.com/SPOORNACHANDRA/practice-tf-module-vpc.git"
   for_each = var.vpc      #this is for how many times i have to iterate
   cidr = each.value["cidr"]   # this cidr whole value is sending to tf-module-vpc
   subnets = each.value["subnets"] # this subnets whole value is sending to tf-module-vpc
