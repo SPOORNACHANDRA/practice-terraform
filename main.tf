16a623c241df83560a391b0ce43c4a40180ed8fa
s poorna chandra <poornanaidu1@gmail.com>
2023-12-07 20:06:07 +0530

    terraform

--- main.tf
+++ main.tf
@@ -2,8 +2,15 @@ module "vpc" {
   source   = "git::https://github.com/SPOORNACHANDRA/practice-tf-module-vpc.git"
   for_each = var.vpc      #this is for how many times i have to iterate
   cidr = each.value["cidr"]
+}
 
 
-output "vpc" {            #this is printing
+
+
+
+
+output "vpc" {
+  #this is printing
   value = module.vpc
+
 }
