ec0fba319908234f03eeca339da096d39ba482b9
s poorna chandra <poornanaidu1@gmail.com>
2023-12-08 07:58:12 +0530

    terraform

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "vpc" {
   source   = "git::https://github.com/SPOORNACHANDRA/practice-tf-module-vpc.git"
   for_each = var.vpc      #this is for how many times i have to iterate
   cidr = each.value["cidr"]
+  subnets = each.value["subnets"]
 }
 
 
