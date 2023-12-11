6cb663193c84289b1e166ea4ad31120d848c36b6
s poorna chandra <poornanaidu1@gmail.com>
2023-12-11 14:06:17 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   vpc_id =lookup(lookup(module.vpc,"main",null ),"vpc_id",null )
-  app_subnets = lookup(lookup(lookup(lookup(module.vpc,"main",null ),"subnets",null ),"app",null),"subnet_ids",null)
+  app_subnets = lookup(lookup(lookup(module.vpc,"main",null ),"subnets",null ),"app",null)
 }
 
