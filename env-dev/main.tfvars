dfccd5129b23ed1192e4e1d18ccad1d28df95dff
s poorna chandra <poornanaidu1@gmail.com>
2023-12-12 17:28:12 +0530

    terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,53 +1,55 @@
 vpc = {
   main = {
-    cidr = "11.0.0.0/16"
+    cidr    = "11.0.0.0/16"
     subnets = {
       public = {
-        public1 = {cidr = "11.0.0.0/24", az = "us-east-1a"}
-        public2 = {cidr = "11.0.1.0/24", az = "us-east-1b"}
+        public1 = { cidr = "11.0.0.0/24", az = "us-east-1a" }
+        public2 = { cidr = "11.0.1.0/24", az = "us-east-1b" }
       }
       app = {
-        app1 = {cidr = "11.0.2.0/24", az = "us-east-1a"}
-        app2 = {cidr = "11.0.3.0/24", az = "us-east-1b"}
+        app1 = { cidr = "11.0.2.0/24", az = "us-east-1a" }
+        app2 = { cidr = "11.0.3.0/24", az = "us-east-1b" }
       }
       db = {
-        db1 = {cidr = "11.0.4.0/24", az = "us-east-1a"}
-        db2 = {cidr = "11.0.5.0/24", az = "us-east-1b"}
+        db1 = { cidr = "11.0.4.0/24", az = "us-east-1a" }
+        db2 = { cidr = "11.0.5.0/24", az = "us-east-1b" }
       }
     }
   }
 }
 
-default_vpc_id = "vpc-033b3aa1458aef7b1"
-default_vpc_cidr = "172.31.0.0/16"
+default_vpc_id             = "vpc-033b3aa1458aef7b1"
+default_vpc_cidr           = "172.31.0.0/16"
 default_vpc_route_table_id = "rtb-0428dc4fcf14ee4d3"
 
 tags = {
-  company_name = "abc tech"
+  company_name  = "abc tech"
   business_unit = "ECommerce"
-  project_Name = "roboshop"
-  cost_center= "ecom_rs"
-  created_by = "terraform"
+  project_Name  = "roboshop"
+  cost_center   = "ecom_rs"
+  created_by    = "terraform"
 }
 env = "dev"
 
-alb ={
-  public ={
-    lb_type = "application"
-    internal  = false
+alb = {
+  public = {
+    lb_type         = "application"
+    internal        = false
     sg_ingress_cidr = ["0.0.0.0/0"]
-    sg_port = 80
+    sg_port         = 80
   }
-  private ={
-    lb_type = "application"
-    internal  = true
-    sg_ingress_cidr = ["172.31.0.0/16","11.0.0.0/16"]
-    sg_port = 80
+  private = {
+    lb_type         = "application"
+    internal        = true
+    sg_ingress_cidr = ["172.31.0.0/16", "11.0.0.0/16"]
+    sg_port         = 80
   }
 }
 
 docdb = {
   main = {
-
+    backup_retention_period = 5
+    preferred_backup_window = "07:00-09:00"
+    skip_final_snapshot     = true
   }
 }
--- main.tf
+++ main.tf
@@ -1,36 +1,41 @@
 module "vpc" {
-  source   = "git::https://github.com/SPOORNACHANDRA/practice-tf-module-vpc.git"
-  for_each = var.vpc      #this is for how many times i have to iterate
-  cidr = each.value["cidr"]
-  subnets = each.value["subnets"]
-  default_vpc_id = var.default_vpc_id
-  default_vpc_cidr = var.default_vpc_cidr
+  source                     = "git::https://github.com/SPOORNACHANDRA/practice-tf-module-vpc.git"
+  for_each                   = var.vpc      #this is for how many times i have to iterate
+  cidr                       = each.value["cidr"]
+  subnets                    = each.value["subnets"]
+  default_vpc_id             = var.default_vpc_id
+  default_vpc_cidr           = var.default_vpc_cidr
   default_vpc_route_table_id = var.default_vpc_route_table_id
-  tags = var.tags
-  env=var.env
+  tags                       = var.tags
+  env                        = var.env
 }
 
 module "alb" {
-  source   = "git::https://github.com/SPOORNACHANDRA/tf-module-alb.git"
-  for_each = var.alb      #this is for how many times i have to iterate
-  lb_type = each.value["lb_type"]
-  internal = each.value["internal"]
+  source          = "git::https://github.com/SPOORNACHANDRA/tf-module-alb.git"
+  for_each        = var.alb      #this is for how many times i have to iterate
+  lb_type         = each.value["lb_type"]
+  internal        = each.value["internal"]
   sg_ingress_cidr = each.value["sg_ingress_cidr"]
-  vpc_id =  each.value["internal"] ? local.vpc_id : var.default_vpc_id
-  subnets =  each.value ["internal"] ?  local.app_subnets: data.aws_subnets.subnets.ids
-  sg_port = each.value["sg_port"]
-  tags = var.tags
-  env=var.env
+  vpc_id          = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+  subnets         = each.value ["internal"] ?  local.app_subnets : data.aws_subnets.subnets.ids
+  sg_port         = each.value["sg_port"]
+  tags            = var.tags
+  env             = var.env
 }
 
 module "docdb" {
-  source   = "git::https://github.com/SPOORNACHANDRA/tf-module-docdb.git"
-  tags = var.tags
-  env=var.env
-  for_each = var.docdb
-  subnet_ids = local.db_subnets
-
+  source                  = "git::https://github.com/SPOORNACHANDRA/tf-module-docdb.git"
+  tags                    = var.tags
+  env                     = var.env
+  for_each                = var.docdb
+  subnet_ids              = local.db_subnets
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  vpc_id                  = vpc_id
+  sg_ingress_cidr         = local.app_subnets_cidr
 }
+
 
 
 
