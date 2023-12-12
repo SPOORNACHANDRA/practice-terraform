566840dc443402a0ce412476f2103c60f7308df2
s poorna chandra <poornanaidu1@gmail.com>
2023-12-12 18:08:11 +0530

    terraform

--- vars.tf
+++ vars.tf
@@ -6,4 +6,10 @@ variable "tags" {}
 variable "env" {}
 variable "alb" {}
 variable "docdb" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+
 
