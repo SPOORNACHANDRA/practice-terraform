26180ccd1a839cc83253256f9e6425e0e362d9f1
s poorna chandra <poornanaidu1@gmail.com>
2023-12-16 11:26:43 +0530

    terraform

--- main.tf
+++ main.tf
@@ -105,6 +105,7 @@ module "app" {
   for_each = var.app
   component=each.key
   port= each.value["port"]
+  instance_type=each.value["instance_type"]
 
   sg_ingress_cidr  = local.app_subnets_cidr
   vpc_id           = local.vpc_id
