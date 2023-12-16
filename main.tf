0a2ce4d1e21d736e751c6415b5f19f3ef1cf8411
s poorna chandra <poornanaidu1@gmail.com>
2023-12-16 11:04:06 +0530

    terraform

--- main.tf
+++ main.tf
@@ -96,15 +96,18 @@ module "rabbitmq" {
 
 module "app" {
   source = "git::https://github.com/SPOORNACHANDRA/tf-module-app.git"
+
   tags   = var.tags
   env    = var.env
   zone_id=var.zone_id
+  ssh_ingress_cidr = var.ssh_ingress_cidr
 
   for_each = var.app
   component=each.name
   port= each.value["port"]
+
   sg_ingress_cidr  = local.app_subnets_cidr
-  ssh_ingress_cidr = var.ssh_ingress_cidr
+  vpc_id           = local.vpc_id
 }
 
 
