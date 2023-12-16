2b667a795e03fecfb15e51750b2903bbc0f376ad
s poorna chandra <poornanaidu1@gmail.com>
2023-12-16 18:27:22 +0530

    terraform

--- main.tf
+++ main.tf
@@ -114,6 +114,8 @@ module "app" {
   sg_ingress_cidr  = local.app_subnets_cidr
   vpc_id           = local.vpc_id
   subnet_ids       = local.app_subnets
+
+  alb_name = lookup(module.alb,"private",null)
 }
 
 
