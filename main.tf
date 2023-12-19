7d0d678efa01997e5c538a84d0ca6806662c76c6
s poorna chandra <poornanaidu1@gmail.com>
2023-12-19 12:03:58 +0530

    terraform

--- main.tf
+++ main.tf
@@ -117,8 +117,8 @@ module "app" {
 
   private_alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
   private_listener =lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
-#  private_alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
-#  private_listener =lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+  public_alb_name = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
+  public_listener =lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
 }
 
 
