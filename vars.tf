6c3f970606b838c7cc0f7e7fa6529a84e8ccde68
s poorna chandra <poornanaidu1@gmail.com>
2024-01-02 20:26:35 +0530

    terraform

--- vars.tf
+++ vars.tf
@@ -1,21 +1,4 @@
-#variable "vpc" {}     #this is variable for module in main.tf in roboshop-terraform-v1 and we are sending to main.tf module
-#variable "default_vpc_id" {}
-#variable "default_vpc_cidr" {}
-#variable "default_vpc_route_table_id" {}
-#variable "tags" {}
-#variable "env" {}
-#variable "alb" {}
-#variable "docdb" {}
-#variable "rds" {}
-#variable "elasticache" {}
-#variable "rabbitmq" {}
-#variable "zone_id" {}
-#variable "app" {}
-#variable "ssh_ingress_cidr" {}
-
-
-
-variable "vpc" {}
+variable "vpc" {}     #this is variable for module in main.tf in roboshop-terraform-v1 and we are sending to main.tf module
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
 variable "default_vpc_route_table_id" {}
@@ -27,5 +10,8 @@ variable "rds" {}
 variable "elasticache" {}
 variable "rabbitmq" {}
 variable "zone_id" {}
-variable "apps" {}
+variable "app" {}
 variable "ssh_ingress_cidr" {}
+
+
+
